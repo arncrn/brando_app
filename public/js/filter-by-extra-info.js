81a276c7a83a8982ef37d116b9928a80afe95da4
Aaron <skilledjuggler@gmail.com>
2021-04-29 11:52:12 -0700

    monkey-patched a filter by pending button

--- public/js/filter-by-extra-info.js
+++ public/js/filter-by-extra-info.js
@@ -1,5 +1,29 @@
 document.addEventListener('DOMContentLoaded', () => {
   let filter = document.getElementById('extra-info-filter');
+  let pendingFilter = document.getElementById('pending-filter');
+
+
+  //working
+  pendingFilter.addEventListener('change', (e) => {
+    let items = document.querySelectorAll('[data-item-id]');
+    if (pendingFilter.checked === true) {
+      pendingFilter.checked = true;
+      items.forEach(item => {
+        let pendingSign = item.querySelector('.pending');
+        if (pendingSign)  {
+          item.style.display = 'flex';
+        } else {
+          item.style.display = 'none';
+        }
+      })
+    } else {
+      pendingFilter.checked = false;
+      items.forEach(item => {
+        item.style.display = 'flex';
+      })
+    }
+  })
+
   filter.addEventListener('input', event => {
     let items = document.querySelectorAll('[data-item-id]');
     items.forEach(item => {
