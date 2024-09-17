f210d5f9f7727a5408fb4ca74b64adf190a86ec1
Aaron Crane <skilledjuggler@gmail.com>
2024-09-17 12:44:23 -0700

    fixed call of missing method

--- controllers/orders.js
+++ controllers/orders.js
@@ -4,20 +4,21 @@ const Clothing = require('../models/items');
 const Customers = require('../models/customers');
 const Packages = require('../models/packages');
 const upload = require('../lib/useMulter');
-const requiresAuthentication = require("../utils/middleware.js").requiresAuthentication;
-const formatDate = require('../lib/format-date.js')
+const requiresAuthentication =
+  require('../utils/middleware.js').requiresAuthentication;
+const formatDate = require('../lib/format-date.js');
 const setTaxPercent = require('../lib/set-tax-percent');
 const calculateTaxAmount = require('../lib/calculate-tax-amount');
 
-orderRouter.get("/", requiresAuthentication, (req, res) => {
+orderRouter.get('/', requiresAuthentication, (req, res) => {
   res.render('order-home');
-})
+});
 
-orderRouter.get("/monthly-profit", (req, res) => {
+orderRouter.get('/monthly-profit', (req, res) => {
   res.render('monthly-profit');
-})
+});
 
-orderRouter.get("/monthly-profits/data", async (req, res) => {
+orderRouter.get('/monthly-profits/data', async (req, res) => {
   const orders = await Orders.getAll();
   const items = await Clothing.getAll();
   const pkgs = await Packages.getAll();
@@ -26,17 +27,18 @@ orderRouter.get("/monthly-profits/data", async (req, res) => {
   let totalRevenue = 0;
   let totalProfit = 0;
 
-  pkgs.forEach(pkg => {
-    itemsInPackage = items.filter(item => item.package_id === pkg.id)
+  pkgs.forEach((pkg) => {
+    itemsInPackage = items.filter((item) => item.package_id === pkg.id);
 
     if (Number(pkg.price) && itemsInPackage.length > 0) {
-      let pricePerItem = (+pkg.price / itemsInPackage.length)
+      let pricePerItem = +pkg.price / itemsInPackage.length;
       pkgPrices[pkg.id] = pricePerItem.toFixed(2);
     }
-  })
+  });
 
-  orders.forEach(order => {
-    if (order.date_sent) order.date_sent = formatDate(order.date_sent.toLocaleDateString())
+  orders.forEach((order) => {
+    if (order.date_sent)
+      order.date_sent = formatDate(order.date_sent.toLocaleDateString());
     order.tax_total = 0;
     order.shipping_total = 0;
     order.base_cost = 0;
@@ -44,14 +46,15 @@ orderRouter.get("/monthly-profits/data", async (req, res) => {
     order.cogs = 0;
     order.total_profit = 0;
 
-    itemsInOrder = items.filter(item => item.order_id === order.id);
+    itemsInOrder = items.filter((item) => item.order_id === order.id);
     order.item_count = itemsInOrder.length;
-    itemsInOrder.forEach(item => {
+    itemsInOrder.forEach((item) => {
       setTaxPercent(item);
       let taxAmount = calculateTaxAmount(item);
-      let shippingCost = Number(item.shipping_cost) || (Number(pkgPrices[item.package_id]) || 0);
-      let cogs = (taxAmount + shippingCost + Number(item.purchase_price));
-      let profit = (Number(item.sold_price) - cogs)
+      let shippingCost =
+        Number(item.shipping_cost) || Number(pkgPrices[item.package_id]) || 0;
+      let cogs = taxAmount + shippingCost + Number(item.purchase_price);
+      let profit = Number(item.sold_price) - cogs;
 
       order.tax_total += taxAmount;
       order.shipping_total += shippingCost;
@@ -63,7 +66,7 @@ orderRouter.get("/monthly-profits/data", async (req, res) => {
       totalCogs += cogs;
       totalRevenue += Number(item.sold_price) || 0;
       totalProfit += profit;
-    })
+    });
 
     order.tax_total = order.tax_total.toFixed(2);
     order.shipping_total = order.shipping_total.toFixed(2);
@@ -71,10 +74,9 @@ orderRouter.get("/monthly-profits/data", async (req, res) => {
     order.sold_total = order.sold_total.toFixed(2);
     order.cogs = order.cogs.toFixed(2);
     order.total_profit = order.total_profit.toFixed(2);
-  })
+  });
 
   res.send(orders).end();
-
 
   // res.render("orders", {
   //   orders,
@@ -84,7 +86,7 @@ orderRouter.get("/monthly-profits/data", async (req, res) => {
   // })
 });
 
-orderRouter.get("/view", requiresAuthentication, async (req, res) => {
+orderRouter.get('/view', requiresAuthentication, async (req, res) => {
   let orders = await Orders.getAll();
   let items = await Clothing.getAll();
   let pkgs = await Packages.getAll();
@@ -99,17 +101,18 @@ orderRouter.get("/view", requiresAuthentication, async (req, res) => {
   //   return 0;
   // })
 
-  pkgs.forEach(pkg => {
-    itemsInPackage = items.filter(item => item.package_id === pkg.id)
+  pkgs.forEach((pkg) => {
+    itemsInPackage = items.filter((item) => item.package_id === pkg.id);
 
     if (Number(pkg.price) && itemsInPackage.length > 0) {
-      let pricePerItem = (+pkg.price / itemsInPackage.length)
+      let pricePerItem = +pkg.price / itemsInPackage.length;
       pkgPrices[pkg.id] = pricePerItem.toFixed(2);
     }
-  })
+  });
 
-  orders.forEach(order => {
-    if (order.date_sent) order.date_sent = formatDate(order.date_sent.toLocaleDateString())
+  orders.forEach((order) => {
+    if (order.date_sent)
+      order.date_sent = formatDate(order.date_sent.toLocaleDateString());
     order.tax_total = 0;
     order.shipping_total = 0;
     order.base_cost = 0;
@@ -117,14 +120,15 @@ orderRouter.get("/view", requiresAuthentication, async (req, res) => {
     order.cogs = 0;
     order.total_profit = 0;
 
-    itemsInOrder = items.filter(item => item.order_id === order.id);
+    itemsInOrder = items.filter((item) => item.order_id === order.id);
     order.item_count = itemsInOrder.length;
-    itemsInOrder.forEach(item => {
+    itemsInOrder.forEach((item) => {
       setTaxPercent(item);
       let taxAmount = calculateTaxAmount(item);
-      let shippingCost = Number(item.shipping_cost) || (Number(pkgPrices[item.package_id]) || 0);
-      let cogs = (taxAmount + shippingCost + Number(item.purchase_price));
-      let profit = (Number(item.sold_price) - cogs)
+      let shippingCost =
+        Number(item.shipping_cost) || Number(pkgPrices[item.package_id]) || 0;
+      let cogs = taxAmount + shippingCost + Number(item.purchase_price);
+      let profit = Number(item.sold_price) - cogs;
 
       order.tax_total += taxAmount;
       order.shipping_total += shippingCost;
@@ -136,7 +140,7 @@ orderRouter.get("/view", requiresAuthentication, async (req, res) => {
       totalCogs += cogs;
       totalRevenue += Number(item.sold_price) || 0;
       totalProfit += profit;
-    })
+    });
 
     order.tax_total = order.tax_total.toFixed(2);
     order.shipping_total = order.shipping_total.toFixed(2);
@@ -144,18 +148,17 @@ orderRouter.get("/view", requiresAuthentication, async (req, res) => {
     order.sold_total = order.sold_total.toFixed(2);
     order.cogs = order.cogs.toFixed(2);
     order.total_profit = order.total_profit.toFixed(2);
-  })
+  });
 
-
-  res.render("orders", {
+  res.render('orders', {
     orders,
     totalRevenue: totalRevenue.toFixed(2),
     totalCogs: totalCogs.toFixed(2),
     totalProfit: totalProfit.toFixed(2),
-  })
-})
+  });
+});
 
-orderRouter.get("/print/:orderId", requiresAuthentication, async (req, res) => {
+orderRouter.get('/print/:orderId', requiresAuthentication, async (req, res) => {
   const orderId = req.params.orderId;
   const order = await Orders.findById(orderId);
   const customer = await Customers.findById(order.customer_id);
@@ -164,28 +167,31 @@ orderRouter.get("/print/:orderId", requiresAuthentication, async (req, res) => {
   let pkgs = await Packages.getAll();
   let pkgPrices = {};
 
-  order.name = customer.name
+  order.name = customer.name;
   order.revenue = 0;
   order.cogs = 0;
   order.profit = 0;
 
-
-  pkgs.forEach(pkg => {
-    itemsInPackage = items.filter(item => item.package_id === pkg.id)
+  pkgs.forEach((pkg) => {
+    itemsInPackage = items.filter((item) => item.package_id === pkg.id);
 
     if (Number(pkg.price) && itemsInPackage.length > 0) {
-      let pricePerItem = (+pkg.price / itemsInPackage.length)
+      let pricePerItem = +pkg.price / itemsInPackage.length;
       pkgPrices[pkg.id] = pricePerItem.toFixed(2);
     }
-  })
+  });
 
-  orderItems.forEach(item => {
+  orderItems.forEach((item) => {
     setTaxPercent(item);
     let taxAmount = calculateTaxAmount(item);
-    let shippingCost = Number(item.shipping_cost) || (Number(pkgPrices[item.package_id]) || 0);
-    let cogs = (taxAmount + shippingCost + Number(item.purchase_price));
+    let shippingCost =
+      Number(item.shipping_cost) || Number(pkgPrices[item.package_id]) || 0;
+    let cogs = taxAmount + shippingCost + Number(item.purchase_price);
     order.cogs += cogs;
-    order.profit += (Number(item.sold_price) - cogs);
+    order.profit += Number(item.sold_price) - cogs;
+    if (isNaN(order.profit)) {
+      console.log('here');
+    }
     order.revenue += Number(item.sold_price);
   });
 
@@ -193,17 +199,17 @@ orderRouter.get("/print/:orderId", requiresAuthentication, async (req, res) => {
   order.profit = order.profit.toFixed(2);
   order.revenue = order.revenue.toFixed(2);
 
-  res.render("print-order", {
+  res.render('print-order', {
     layout: false,
-    order
-  })
-})
+    order,
+  });
+});
 
-orderRouter.get("/view/:orderId", requiresAuthentication, async (req, res) => {
+orderRouter.get('/view/:orderId', requiresAuthentication, async (req, res) => {
   const orderId = req.params.orderId;
   const order = await Orders.findById(orderId);
 
-  let items = (await Orders.getOrderItems(orderId)).map(item => {
+  let items = (await Clothing.findInOrder(orderId)).map((item) => {
     setTaxPercent(item);
     let taxAmount = calculateTaxAmount(item);
     let totalPrice = (+item.purchase_price + taxAmount).toFixed(2);
@@ -214,7 +220,7 @@ orderRouter.get("/view/:orderId", requiresAuthentication, async (req, res) => {
     filters: [order.customer_id, order.name],
     helpers: {
       reserved: (soldTo, soldPrice) => {
-        return soldTo && !Number(soldPrice) > 0
+        return soldTo && !Number(soldPrice) > 0;
       },
       isInStock: function (bool) {
         return bool === true;
@@ -233,45 +239,49 @@ orderRouter.get("/view/:orderId", requiresAuthentication, async (req, res) => {
       },
       isForMen: function (gender) {
         return gender == 'men';
-      }
-    }
+      },
+    },
   });
+});
 
-})
+orderRouter.get('/add', requiresAuthentication, (req, res) => {
+  res.render('create-order');
+});
 
-orderRouter.get("/add", requiresAuthentication, (req, res) => {
-  res.render("create-order");
-})
+orderRouter.post(
+  '/add',
+  upload.none(),
+  requiresAuthentication,
+  async (req, res) => {
+    const orderData = req.body;
+    for (key in orderData) {
+      orderData[key] = orderData[key] || null;
+    }
 
-orderRouter.post("/add", upload.none(), requiresAuthentication, async (req, res) => {
-  const orderData = req.body;
-  for (key in orderData) {
-    orderData[key] = orderData[key] || null;
+    const createdOrder = await Orders.create(orderData);
+
+    if (createdOrder) {
+      res.redirect('/orders/view');
+    } else {
+      res.render('create-order');
+    }
   }
+);
 
-  const createdOrder = await Orders.create(orderData);
-
-  if (createdOrder) {
-    res.redirect("/orders/view");
-  } else {
-    res.render("create-order");
-  }
-})
-
-orderRouter.get("/edit/:orderId", requiresAuthentication, async (req, res) => {
+orderRouter.get('/edit/:orderId', requiresAuthentication, async (req, res) => {
   let orderId = req.params.orderId;
   let order = await Orders.findById(orderId);
   if (order.date_sent) {
     order.date_sent = formatDate(order.date_sent.toLocaleDateString());
   }
 
-  res.render("partials/order-row", {
+  res.render('partials/order-row', {
     layout: false,
-    order: order
+    order: order,
   });
-})
+});
 
-orderRouter.post("/edit/:orderId", requiresAuthentication, async (req, res) => {
+orderRouter.post('/edit/:orderId', requiresAuthentication, async (req, res) => {
   let orderId = req.params.orderId;
   let successfulUpdate = await Orders.update(req.body);
 
@@ -289,22 +299,21 @@ orderRouter.post("/edit/:orderId", requiresAuthentication, async (req, res) => {
     let pkgs = await Packages.getAll();
     let pkgPrices = {};
 
-
-    pkgs.forEach(pkg => {
-      itemsInPackage = items.filter(item => item.package_id === pkg.id)
+    pkgs.forEach((pkg) => {
+      itemsInPackage = items.filter((item) => item.package_id === pkg.id);
 
       if (Number(pkg.price) && itemsInPackage.length > 0) {
-        let pricePerItem = (+pkg.price / itemsInPackage.length)
+        let pricePerItem = +pkg.price / itemsInPackage.length;
         pkgPrices[pkg.id] = pricePerItem.toFixed(2);
       }
-    })
+    });
 
-    itemsInOrder.forEach(item => {
+    itemsInOrder.forEach((item) => {
       setTaxPercent(item);
       let taxAmount = calculateTaxAmount(item);
-      let shippingCost = Number(pkgPrices[item.package_id]) || 0
-      let cogs = (taxAmount + shippingCost + Number(item.purchase_price));
-      let profit = (Number(item.sold_price) - cogs)
+      let shippingCost = Number(pkgPrices[item.package_id]) || 0;
+      let cogs = taxAmount + shippingCost + Number(item.purchase_price);
+      let profit = Number(item.sold_price) - cogs;
 
       order.tax_total += taxAmount;
       order.shipping_total += shippingCost;
@@ -312,7 +321,7 @@ orderRouter.post("/edit/:orderId", requiresAuthentication, async (req, res) => {
       order.sold_total += Number(item.sold_price) || 0;
       order.cogs += cogs;
       order.total_profit += profit;
-    })
+    });
 
     order.item_count = itemsInOrder.length;
 
@@ -330,10 +339,9 @@ orderRouter.post("/edit/:orderId", requiresAuthentication, async (req, res) => {
     res.render(`partials/edited-order-row`, {
       layout: false,
       order: order,
-    })
+    });
   }
-})
-
+});
 
 orderRouter.get('/orderitems/:orderId', async (req, res) => {
   const orderId = req.params.orderId;
@@ -343,33 +351,38 @@ orderRouter.get('/orderitems/:orderId', async (req, res) => {
   let pkgs = await Packages.getAll();
   let pkgPrices = {};
 
-  pkgs.forEach(pkg => {
-    itemsInPackage = items.filter(item => item.package_id === pkg.id)
+  pkgs.forEach((pkg) => {
+    itemsInPackage = items.filter((item) => item.package_id === pkg.id);
 
     if (Number(pkg.price) && itemsInPackage.length > 0) {
-      let pricePerItem = (+pkg.price / itemsInPackage.length)
+      let pricePerItem = +pkg.price / itemsInPackage.length;
       pkgPrices[pkg.id] = pricePerItem.toFixed(2);
     }
-  })
+  });
 
   let sumOfItems = 0;
   let shippingCost = 0;
   let revenue = 0;
   let profit = 0;
 
-  orderItems.forEach(item => {
+  orderItems.forEach((item) => {
     setTaxPercent(item);
     let taxAmount = calculateTaxAmount(item);
     let totalPrice = taxAmount + Number(item.purchase_price);
     item.total_price = totalPrice.toFixed(2);
     item.tax_amount = taxAmount.toFixed(2);
-    item.shipping_cost = Number(item.shipping_cost) || (Number(pkgPrices[item.package_id]) || 0);
-    item.profit = (Number(item.sold_price) - totalPrice - Number(item.shipping_cost)).toFixed(2);
+    item.shipping_cost =
+      Number(item.shipping_cost) || Number(pkgPrices[item.package_id]) || 0;
+    item.profit = (
+      Number(item.sold_price) -
+      totalPrice -
+      Number(item.shipping_cost)
+    ).toFixed(2);
     sumOfItems += totalPrice;
     revenue += Number(item.sold_price);
     profit += Number(item.profit);
     shippingCost += item.shipping_cost;
-  })
+  });
 
   res.json({
     items: orderItems,
@@ -377,7 +390,7 @@ orderRouter.get('/orderitems/:orderId', async (req, res) => {
     shippingCost: shippingCost.toFixed(2),
     revenue: revenue.toFixed(2),
     profit: profit.toFixed(2),
-  })
-})
+  });
+});
 
 module.exports = orderRouter;
